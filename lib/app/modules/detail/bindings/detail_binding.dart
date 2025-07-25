9d6abe0237416689aa11e9e510e8b2da606378aa
handikaa <dikadjati94@gmail.com>
2025-07-25 12:52:43 +0700

    FIX: watchController not found

--- lib/app/modules/detail/bindings/detail_binding.dart
+++ lib/app/modules/detail/bindings/detail_binding.dart
@@ -1,14 +1,18 @@
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
-import 'package:my_streaming/app/data/repository/movie/movie_repository.dart';
-import 'package:my_streaming/app/data/repository/tv_shows/tv_shows_repository.dart';
 
+import '../../../data/repository/movie/movie_repository.dart';
+import '../../../data/repository/tv_shows/tv_shows_repository.dart';
+import '../../watch_list/controllers/watch_list_controller.dart';
 import '../controllers/detail_controller.dart';
 
 class DetailBinding extends Bindings {
   @override
   void dependencies() {
     final inject = GetIt.instance;
+
+    Get.lazyPut(() => WatchListController());
+
     Get.lazyPut<DetailController>(
       () => DetailController(
         tvShowsRepositoryImpl: TvShowsRepositoryImpl(dio: inject()),
